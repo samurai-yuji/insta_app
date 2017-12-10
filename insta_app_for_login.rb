ddb1400fb6dc76b65bd4fcd11b7b9cd6a35215dc
Yuji Nishida <yuji.nishida@example.com>
2017-12-10 12:48:09 +0900

    Added the sinatra app to get user id from instargram over its api for login.

---
+++ insta_app_for_login.rb
@@ -0,0 +1,47 @@
+require 'instagram'
+require 'hashie'
+require 'sinatra'
+require 'httpclient'
+require 'json'
+require 'uri'
+
+class App < Sinatra::Base
+
+	configure :development do
+		enable :sessions
+	end
+
+	get "/login" do
+		#Fixed : properties are not implicitly shared over a session
+		#        need to enable session and use session variables
+		#@client_id = ENV["INSTAGRAM_CLIENT_ID"]
+		session[:cid] = ENV["INSTAGRAM_CLIENT_ID"]
+		session[:ruri] = ENV["INSTAGRAM_REDIRECT_URI"]
+	
+		uri = URI.encode("https://api.instagram.com/oauth/authorize/?client_id=#{session[:cid]}&redirect_uri=#{session[:ruri]}&response_type=code&scope=basic+public_content+follower_list+comments+relationships+likes")
+
+		#Fixed : form does not work (I do not understand why)
+		#return "<form action='#{uri}' method='get'><input type=submit>instagram login</input></form>"
+		return "<a href='#{uri}'>instagram login</a>"
+	end
+	
+	get "/redirect_from_instagram" do
+		code = params["code"]
+	
+		client_secret = ENV["INSTAGRAM_CLIENT_SECRET"]
+	
+		query = {:client_id => session[:cid], :client_secret => client_secret, :grant_type => "authorization_code", :redirect_uri => session[:ruri], :code => code}
+
+		#Fixed : Here post is needed instead of get
+		#json = HTTPClient.new().get_content("https://api.instagram.com/oauth/access_token",query)
+		json = HTTPClient.new().post_content("https://api.instagram.com/oauth/access_token",query)
+		content = JSON.parse(json)
+		
+		#Fixed : symbols are not implicitly generated according to string keys
+		#return content[:user][:id]
+		return content["user"]["id"]
+	end
+end
+
+App.run!
+
