64d55682b7bc3db13d073e0a973bb0dd36242715
Yuji Nishida <yuji.nishida@example.com>
2017-12-10 12:47:34 +0900

    Forgot to add uri.

--- insta_app.rb
+++ insta_app.rb
@@ -5,7 +5,7 @@
 # https://api.instagram.com/oauth/authorize/?client_id=CLIENT_ID&redirect_uri=http://localhost&response_type=code&scope=basic+public_content+follower_list+comments+relationships+likes
 # http://localhost/?code=CODE
 #
-# curl -F 'client_id=CLIENT_ID' -F 'client_secret=SECRET_KEY' -F 'grant_type=authorization_code' -F 'redirect_uri=http://localhost' -F 'code=CODE'
+# curl -F 'client_id=CLIENT_ID' -F 'client_secret=SECRET_KEY' -F 'grant_type=authorization_code' -F 'redirect_uri=http://localhost' -F 'code=CODE' https://api.instagram.com/oauth/access_token
 #{"access_token": ACCESS_TOKEN, "user": {"id": "xxxxxx", "username": "nishidy", "profile_picture": "", "full_name": "", "bio": "", "website": "", "is_business": false}}
 
 require 'instagram'
